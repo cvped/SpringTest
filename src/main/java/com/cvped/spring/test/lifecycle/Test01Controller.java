fde2b32a796b747632e9dc5ac9b6068670653306
cvped <stay488@gmail.com>
2024-10-31 18:47:32 +0900

    Update Test01Controller.java

--- src/main/java/com/cvped/spring/test/lifecycle/Test01Controller.java
+++ src/main/java/com/cvped/spring/test/lifecycle/Test01Controller.java
@@ -9,7 +9,7 @@ public class Test01Controller {
 
 	@ResponseBody
 	@RequestMapping("/lifecycle/test01/1")
-	public String stringTest01() {
+	public String stringTest0() {
 		return "<h2>테스트 프로젝트 완성";
 	}
 }
